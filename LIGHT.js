29d009f671d8bee59fd3254fe73292395476de8a
vihaannbeast <85434824+vihaannbeast@users.noreply.github.com>
2023-02-27 20:48:10 +0400

    Add files via upload

---
+++ LIGHT.js
@@ -0,0 +1,30 @@
+Status = "";
+Light_Bulb_image = "";
+objects = [];
+
+function preload(){
+    Light_Bulb_image = loadImage("Light Bulb.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(Light_Bulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
